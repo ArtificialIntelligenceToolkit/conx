63e08e4430fd04f56df795d4450c157620b1be7b
Doug Blank <dblank@cs.brynmawr.edu>
2016-07-17 15:40:33 -0400

    Version 1.0.2

--- conx/__init__.py
+++ conx/__init__.py
@@ -17,7 +17,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301  USA
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 from .network import Network
 from .srn import SRN
