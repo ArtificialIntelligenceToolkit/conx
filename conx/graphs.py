099b330cef47298035ae00839c25c4fc355b0d88
Jim Marshall <jbmarshall@bitbucket.org>
2017-11-01 20:06:23 -0400

    fixed plot_activations, added colorbars

--- conx/graphs.py
+++ conx/graphs.py
@@ -74,46 +74,72 @@ def plot(lines, width=8.0, height=4.0, xlabel="time", ylabel=""):
     #plt.close(fig)
     #return SVG(svg.decode())
 
+
 def plot_activations(net, output_layer="output", output_index=0,
                      input_layer="input",input_index1=0, input_index2=1,
-                     colormap=None, default_input_value=0, resolution=0.1):
+                     colormap=None, default_input_value=0, resolution=None,
+                     act_range=None):
     if colormap is None: colormap = get_colormap()
     if plt is None:
         raise Exception("matplotlib was not loaded")
-    act_range = net[input_layer].minmax
-    if act_range is None:
-        slice = (-1, 1, resolution)
+    
+    # FIXME: why is input_layer even an argument here, since
+    # propagate_to always requires starting from "input"???
+    assert input_layer == 'input', "for now, input_layer must be 'input'"
+
+    default_ranges = {'sigmoid': (0, 1),
+                      'tanh': (-1, 1),
+                      'relu': (0, 2),
+                      'linear': (-2, 2),
+                      'softmax': (0, 1)}
+
+    if act_range is not None:
+        act_min, act_max = act_range
+    elif net[input_layer].activation in default_ranges:
+        act_min, act_max = default_ranges[net[input_layer].activation]
+    # elif net[input_layer].minmax is not None:
+    #     act_min, act_max = net[input_layer].minmax
     else:
-        slice = (act_range[0], act_range[1], resolution)
-    min1, max1, step1 = slice
-    min2, max2, step2 = slice
-    resolution1 = int((max1 - min1) / step1)
-    resolution2 = int((max2 - min2) / step2)
-    mat = np.zeros((resolution1, resolution2))
-    for i1 in range(resolution1):
-        for i2 in range(resolution2):
-            input1 = i1/resolution1 * (max1 - min1) + min1
-            input2 = i2/resolution2 * (max2 - min2) + min2
+        act_min, act_max = -1, 1
+
+    if resolution is None:
+        default_pixels = 50
+        resolution = (act_max - act_min) / default_pixels
+    xmin, xmax, xstep = act_min, act_max, resolution
+    ymin, ymax, ystep = act_min, act_max, resolution
+    xspan = xmax - xmin
+    yspan = ymax - ymin
+    xpixels = int(xspan/xstep)+1
+    ypixels = int(yspan/ystep)+1
+    mat = np.zeros((ypixels, xpixels))
+    for row in range(ypixels):
+        for col in range(xpixels):
+            # (x,y) corresponds to lower left corner point of pixel
+            x = xmin + xstep*col
+            y = ymin + ystep*row
             vector = net[input_layer].make_dummy_vector(default_input_value)
-            vector[input_index1] = input1
-            vector[input_index2] = input2
-            mat[i1, i2] = net.propagate_to(output_layer, vector,
-                                           visualize=False)[output_index]
-    figure = plt.matshow(mat, origin="lower", cmap=colormap)
-    plt.title("Activation of %s[%s]" % (output_layer, output_index))
-    plt.xlabel("%s[%s]" % (input_layer, input_index1))
-    plt.ylabel("%s[%s]" % (input_layer, input_index2))
-    figure.axes.set_xticks([0, 0.25 * resolution1, 0.50 * resolution1, 0.75 * resolution1])
-    figure.axes.set_xticklabels([min1,
-                                 (max1 - min1) * 0.25,
-                                 (max1 - min1) * 0.50,
-                                 (max1 - min1) * 0.75,])
-    figure.axes.set_yticks([0, 0.25 * resolution2, 0.50 * resolution2, 0.75 * resolution2])
-    figure.axes.set_yticklabels([min2,
-                                 (max2 - min2) * 0.25,
-                                 (max2 - min2) * 0.50,
-                                 (max2 - min2) * 0.75,])
+            vector[input_index1] = x
+            vector[input_index2] = y
+            mat[row,col] = net.propagate_to(output_layer, vector, visualize=False)[output_index]
+    # print("matrix is:")
+    # for row in mat:
+    #     print(''.join(['%.6f ' % x for x in row]))
+    fig, ax = plt.subplots()
+    cax = ax.imshow(mat, origin='lower', cmap=colormap)
+    ax.set_title("Activation of %s[%s]" % (output_layer, output_index))
+    ax.set_xlabel("%s[%s]" % (input_layer, input_index1))
+    ax.set_ylabel("%s[%s]" % (input_layer, input_index2))
+    ax.xaxis.tick_bottom()
+    ax.xaxis.set_ticks([i*(xpixels-1)/4 for i in range(5)])
+    ax.xaxis.set_ticklabels([xmin+i*xspan/4 for i in range(5)])
+    ax.yaxis.set_ticks([i*(ypixels-1)/4 for i in range(5)])
+    ax.yaxis.set_ticklabels([ymin+i*yspan/4 for i in range(5)])
+    # added a colorbar. FIXME: ideally, maximum red should always map to
+    # act_min and maximum blue should map to act_max, but i don't know how
+    # to do this. matplotlib automatically rescales the colorbar as it likes.
+    cbar = fig.colorbar(cax)
     plt.show()
+
     ##plt.ylim([min2,max2])
     ##plt.xlim([min1,max1])
     ## Turn off ticks
