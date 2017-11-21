73731d1c9154e473ff1af0c4e9264e9b8b376e4a
Doug Blank <doug.blank@gmail.com>
2017-11-21 06:14:31 -0500

    Override CSS in read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,8 +128,12 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
+html_static_path = ['_static']
+# html_theme_options = {}
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
 
 # -- Options for HTMLHelp output ------------------------------------------
 
