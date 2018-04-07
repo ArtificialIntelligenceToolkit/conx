f63a9052e71ff9025f43b901ceade6470904b4d3
Douglas Blank <doug.blank@gmail.com>
2018-04-07 14:58:47 -0400

    Allow cx.download() to save in subdir

--- conx/utils.py
+++ conx/utils.py
@@ -475,7 +475,11 @@ def download(url, directory="./", force=False, unzip=True, filename=None):
     Using cached ...
     """
     result = urlparse(url)
-    filename = filename if filename is not None else result.path.split("/")[-1]
+    if filename is None:
+        filename = result.path.split("/")[-1]
+    elif "/" in filename:
+        subdirs, _ = filename.rsplit("/", 1)
+        os.makedirs(subdirs, exist_ok=True)
     file_path = os.path.join(directory, filename)
     ## First, download the file:
     if not os.path.isfile(file_path) or force:
