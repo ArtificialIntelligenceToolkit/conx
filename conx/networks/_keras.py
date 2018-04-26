03b3be706c6bc6c62e02615516b5a84470f23690
Jim Marshall <jbmarshall@bitbucket.org>
2018-04-26 13:06:33 -0700

    added VGG preprocessing and postprocessing

--- conx/networks/_keras.py
+++ conx/networks/_keras.py
@@ -1,24 +1,21 @@
 import os
+import numpy as np
 
 import keras.applications
 from keras.utils import get_file
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 
 from ..utils import import_keras_model
 
-def vgg16(*args, **kwargs):
-    WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1'
-    WEIGHTS_NAME = 'vgg16_weights_tf_dim_ordering_tf_kernels.h5'
-    if "weights" not in kwargs:
-        kwargs["weights"] = None
-    model = keras.applications.VGG16(**kwargs)
-    network = import_keras_model(model, "VGG16")
-    weights_path = get_file(
-        WEIGHTS_NAME,
-        os.path.join(WEIGHTS_PATH, WEIGHTS_NAME),
-        cache_subdir='models',
-        file_hash='64373286793e3c8b2b4e3219cbf3544b')
-    network.load_weights(*weights_path.rsplit("/", 1))
-    return network
+def vgg_preprocess(input):
+    batch = np.array(input).reshape((1, 224, 224, 3))
+    assert np.min(batch) >= 0 and np.max(batch) <= 1
+    batch *= 255
+    b =  preprocess_input(batch)
+    return b[0].tolist()
+
+def vgg_decode(probabilities, top=5):
+    return decode_predictions(np.array(probabilities).reshape((1,1000)), top=top)[0]
 
 def vgg16(*args, **kwargs):
     WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1'
@@ -33,6 +30,8 @@ def vgg16(*args, **kwargs):
         cache_subdir='models',
         file_hash='64373286793e3c8b2b4e3219cbf3544b')
     network.load_weights(*weights_path.rsplit("/", 1))
+    network.preprocess = vgg_preprocess
+    network.postprocess = vgg_decode
     return network
 
 def vgg19(*args, **kwargs):
@@ -48,4 +47,7 @@ def vgg19(*args, **kwargs):
         cache_subdir='models',
         file_hash='253f8cb515780f3b799900260a226db6')
     network.load_weights(*weights_path.rsplit("/", 1))
+    network.preprocess = vgg_preprocess
+    network.postprocess = vgg_decode
     return network
+
