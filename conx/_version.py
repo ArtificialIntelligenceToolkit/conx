935f634a26688fcb7a36ae8825dc073e1d2a8c1c
Douglas Blank <doug.blank@gmail.com>
2018-08-20 09:47:53 -0400

    Release 3.7.4

--- conx/_version.py
+++ conx/_version.py
@@ -17,6 +17,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301  USA
 
-__version__ = "3.7.3"
+__version__ = "3.7.4"
 VERSION = tuple([(int(v) if v.isdigit() else v)
                  for v in __version__.split(".")])
